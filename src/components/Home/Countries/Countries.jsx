5a02dc33a2fb91933eb575eaa59af3172f7938e1
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-03 02:50:38 +0600

    countries ul map and use loader data

--- src/components/Home/Countries/Countries.jsx
+++ src/components/Home/Countries/Countries.jsx
@@ -1,10 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Countries = () => {
+    const countries = useLoaderData();
     return (
         <div>
-            <h3>All Countries</h3>
+            <h3>All Countries: {countries.length}</h3>
+            <ul>
+                {
+                    // eslint-disable-next-line react/jsx-key
+                    countries.map(country => <li>{country.name.common}</li>)
+                }
+            </ul>
         </div>
     );
 };
